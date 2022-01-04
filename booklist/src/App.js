ded477d237b98306fafea5b44cb2193aeac0cc72
trevor little <littlehome80@gmail.com>
2022-01-04 11:37:44 -0700

    first commit

---
+++ booklist/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import BookForm from './components/BookForm';
+import Book from './components/Book';
+
+function App() {
+  return (
+    <div className="App">
+      <BookForm />
+      <Book />
+    </div>
+  );
+}
+
+export default App;
