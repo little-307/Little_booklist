cf8d5ab0c227c148cdd9716ceb7f8143b665f573
trevor little <littlehome80@gmail.com>
2022-01-04 16:45:55 -0700

    i forgot to start a new branch

---
+++ booklist/data/BookData.js
@@ -0,0 +1,14 @@
+let BookData = [
+    {
+        "title": "Moby Dick",
+        "author": "Herman Melville",
+        "synopsis": "The White Whale"
+    },
+    {
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "synopsis": "I prefer Zelda"
+    }
+]
+
+export default BookData
