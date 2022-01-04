348c7ff98cb1da3d6b5f3e90c8b1fa5ebf8c8a04
trevor little <littlehome80@gmail.com>
2022-01-04 16:01:21 -0700

    local-state

--- booklist/src/components/BookForm.jsx
+++ booklist/src/components/BookForm.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
-import {Form} from 'react-bootstrap';
+import React, {useState} from 'react';
+import { Form, Button } from 'react-bootstrap';
+import Book from './Book';
 
-function BookForm() {
+function BookForm({setBook}) {
+    
+    const [response, setResponse] = useState()
+
+    const updateField = (e) => {
+        setResponse ({
+            ...response,
+            [e.target.name]: e.target.value
+        })
+        console.log(response)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // let updateContentArr = [
+        //     ...content, response];
+    setBook({response})
+    //         if (response.title) {
+    //             dispatch(addContent(updateContentArr))
+    //             setResponse({}) // empty the field after submit.
+    //         }
+    }
+
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" placeholder="Title" />
+                        <Form.Control type="text" placeholder="Title" name="title" onChange={updateField}/>
                 </Form.Group>
                 <Form.Group className="mb-3">
                         <Form.Label>Author</Form.Label>
-                        <Form.Control type="text" placeholder="Author" />
+                        <Form.Control type="text" placeholder="Author" name="author" onChange={updateField}/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Synopsis</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control as="textarea" rows={3} name="synopsis" onChange={updateField}/>
                 </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
             </Form>
         </div>
     )
